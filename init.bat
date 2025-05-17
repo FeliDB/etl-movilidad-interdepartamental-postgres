b1954d105678b05530e8ddf8d5cd89014765edcd
Felipe Diaz Bertero <felipediazb16@gmail.com>
2025-05-17 18:18:37 -0300

    Commit

--- init.bat
+++ init.bat
@@ -1,3 +1,11 @@
-docker compose exec -it superset superset fab create-admin --username admin --firstname Superset --lastname Admin --email admin@superset.com --password admin
+echo "Inicializamos el usuario de superset"
+docker compose exec -it superset superset fab create-admin \
+              --username admin \
+              --firstname Superset \
+              --lastname Admin \
+              --email admin@superset.com \
+              --password admin
+echo "Migramos la base de datos"
 docker compose exec -it superset superset db upgrade
+echo "Seteamos los Roles"
 docker compose exec -it superset superset init
